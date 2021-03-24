0d152722b9aff959dc5ab9524fa8f4fa66fce0d2
Likhith Aalla <likhithaalla@Likhiths-MacBook-Pro.local>
2021-03-24 17:46:47 -0500

    path change

--- .github/xcode-version-increment.sh
+++ .github/xcode-version-increment.sh
@@ -6,7 +6,7 @@ INFO_PLIST_EXT="${INFO_PLIST}.plist"
 BUILD_NUMBER="$(defaults read $INFO_PLIST CFBundleVersion)"
 ((BUILD_NUMBER+=1))
 VERSION_NUMBER=1.0.$BUILD_NUMBER
-
+pwd
 sed write $INFO_PLIST CFBundleShortVersionString $VERSION_NUMBER
 defaults write $INFO_PLIST CFBundleVersion $BUILD_NUMBER
 plutil -convert xml1 $INFO_PLIST_EXT
