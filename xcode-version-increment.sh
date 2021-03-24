a86cd6acf00e82e5817f876ab5903eced72db7f4
Likhith Aalla <likhithaalla@Likhiths-MacBook-Pro.local>
2021-03-24 17:32:30 -0500

    revamp

---
+++ xcode-version-increment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+INFO_PLIST="${HOME}/cloneapps/DTAS/ios/App/App/Info"
+INFO_PLIST_EXT="${INFO_PLIST}.plist"
+BUILD_NUMBER="$(defaults read $INFO_PLIST CFBundleVersion)"
+((BUILD_NUMBER+=1))
+VERSION_NUMBER=1.0.$BUILD_NUMBER
+
+defaults write $INFO_PLIST CFBundleShortVersionString $VERSION_NUMBER
+defaults write $INFO_PLIST CFBundleVersion $BUILD_NUMBER
+plutil -convert xml1 $INFO_PLIST_EXT
+
+echo "Released $VERSION_NUMBER"
+echo "Incremented build number to $BUILD_NUMBER"
