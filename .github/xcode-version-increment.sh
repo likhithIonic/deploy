1bee148099d56115a05bf57bd2719cc78c10184b
Likhith Aalla <likhithaalla@Likhiths-MacBook-Pro.local>
2021-03-24 19:47:18 -0500

    test

--- .github/xcode-version-increment.sh
+++ .github/xcode-version-increment.sh
@@ -3,11 +3,11 @@
 
 INFO_PLIST="ios/App/App/Info"
 INFO_PLIST_EXT="${INFO_PLIST}.plist"
-BUILD_NUMBER="$(defaults read $INFO_PLIST CFBundleVersion)"
+BUILD_NUMBER=2
 ((BUILD_NUMBER+=1))
 ls -l
 VERSION_NUMBER=1.0.$BUILD_NUMBER
-sed write $INFO_PLIST CFBundleShortVersionString $VERSION_NUMBER
+defaults write $INFO_PLIST CFBundleShortVersionString $VERSION_NUMBER
 defaults write $INFO_PLIST CFBundleVersion $BUILD_NUMBER
 plutil -convert xml1 $INFO_PLIST_EXT
 
